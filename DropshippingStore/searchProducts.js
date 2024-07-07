bf069961619a10ef2a194b65ee3ba56aafc5e27c
tamaranaj <najdovska18@gmail.com>
2024-07-08 01:09:55 +0200

    update

--- DropshippingStore/searchProducts.js
+++ DropshippingStore/searchProducts.js
@@ -5,9 +5,31 @@ document.addEventListener('DOMContentLoaded', async function(){
     let copyData = [...data]
     searchInputService.addSearchEvent(copyData)
     itemsInCart.displayItems()
+
+    let user = isAuthenticated()
+    if(!user){
+    document.getElementById('LogOutBtn').style.display = 'none'
+    document.getElementById('signUp').style.display = 'block'
+    document.getElementById('LoginBtn').style.display = 'block'
+    }else{
+    document.getElementById('LogOutBtn').style.display = 'block'
+    document.getElementById('signUp').style.display = 'none'
+    document.getElementById('LoginBtn').style.display = 'none'
+    }
 })
 
 
+
+function isAuthenticated() {
+    return localStorage.getItem("user-creds") !== null;
+}
+
+const logOutBtn = document.getElementById("LogOutBtn");
+logOutBtn.addEventListener("click", () => {
+    localStorage.removeItem("user-creds");
+    localStorage.removeItem("cart-items");
+    document.location.href = "../index.html";
+});
 
 const itemsInCart = {
     cart: document.getElementById("cart-count"),
@@ -252,3 +274,6 @@ const searchInputService = {
     }
 
 }
+
+
+
