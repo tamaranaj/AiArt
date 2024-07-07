7b0c62e14b999036a52281d4530ec98fc8c7879a
tamaranaj <najdovska18@gmail.com>
2024-07-08 01:14:02 +0200

    update

--- DropshippingStore/main.js
+++ DropshippingStore/main.js
@@ -6,7 +6,7 @@ function showSlide(index) {
     const slides = document.querySelector('.slides');
     const totalSlides = document.querySelectorAll('.slide').length;
     currentSlide = (index + totalSlides) % totalSlides;
-    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+    // slides.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 
 function changeSlide(step) {
