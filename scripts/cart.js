c2f254f2a32b7049a68796891094e062a6e079c6
tamaranaj <najdovska18@gmail.com>
2024-07-08 11:06:17 +0200

    update

--- scripts/cart.js
+++ scripts/cart.js
@@ -3,10 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const totalAmount = document.getElementById("totalAmount");
   const checkoutBtn = document.getElementById("checkoutBtn");
   let items = JSON.parse(localStorage.getItem("cart-items")) || [];
-  // if(items){
-  //   let imeges = items.map(i=>{if(i.imageUrl.charAt[0]=="i"){i.imageUrl=`../${imageUrl}`}})
-  //     return imeges
-  // }
+
   
   function isAuthenticated() {
     return localStorage.getItem("user-creds") !== null;
